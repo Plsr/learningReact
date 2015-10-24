c79ab85bc25578363b7d56765046dd70dbd31282
plsr <christianpoplawski@gmail.com>
2015-10-24 19:44:43 +0200

    Added simple counter

---
+++ Counter.jsx
@@ -0,0 +1,26 @@
+/** @jsx React.DOM */
+
+'use strict'
+
+var React = require('react')
+
+module.exports = React.createClass ({
+  incrementCount: function() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  getInitialState: function() {
+    return {
+      count: 0
+    }
+  },
+  render: function() {
+    return (
+      <div class="counter">
+        <h1> Count: {this.state.count}</h1>
+        <button type="button" onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+});
--- index.jsx
+++ index.jsx
@@ -3,10 +3,17 @@
 'use strict'
 
 var React = require('react')
-var Hello = require('./Hello')
 var ReactDOM = require('react-dom')
+var Hello = require('./Hello')
+var Counter = require('./Counter')
+
 
 ReactDOM.render(
   <Hello />,
+  document.getElementById('hello')
+)
+
+ReactDOM.render(
+  <Counter />,
   document.getElementById('content')
 )
